42145f3e11b6f8d1686fba2bb926b30b9fc7ee5a
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2023-12-27 21:53:33 -0300

    27/12/2023

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,7 @@
+const squares = document.querySelectorAll(".square")
 let counter = 0
+
 window.onload = () => {
-    const squares = document.querySelectorAll(".square")
     setInfoSquare.bind(squares)()
     show.bind(squares)()
 }
@@ -9,7 +10,6 @@ window.onload = () => {
 function setInfoSquare() {
     const arr = links()
     for (let idx = 0; idx < this.length; idx++) {
-        this[idx].setAttribute("data-num", idx)
         this[idx].setAttribute("data-value", arr[idx].value)
     }
 }
@@ -48,23 +48,21 @@ const show = function () {
     this.forEach(elm => elm.addEventListener("click", turn))
 }
 function turn() {
-    const cards = document.querySelectorAll(".square")
     this.removeEventListener("click", turn)
     this.classList.add("show")
     this.classList.remove("hide")
     const turned = document.querySelectorAll(".show")
-    setTimeout(() => {
-        this.innerHTML = srcImage(this.dataset.value).src
-    }, 100)
+    this.innerHTML = srcImage(this.dataset.value).src
+
     if (counter == 1) {
-        cards.forEach(elm => elm.removeEventListener("click", turn))
+        squares.forEach(elm => elm.removeEventListener("click", turn))
         if (turned[0].dataset.value == turned[1].dataset.value) {
             setTimeout(() => {
                 turned[0].classList.remove("show")
                 turned[1].classList.remove("show")
                 turned[0].classList.add("discovered")
                 turned[1].classList.add("discovered")
-                cards.forEach(elm => elm.addEventListener("click", turn))
+                squares.forEach(elm => elm.addEventListener("click", turn))
             }, 400)
         } else {
             setTimeout(() => {
@@ -74,11 +72,16 @@ function turn() {
                 turned[1].classList.add("hide")
                 turned[0].innerHTML = srcImage(undefined).icon
                 turned[1].innerHTML = srcImage(undefined).icon
-                cards.forEach(elm => elm.addEventListener("click", turn))
+                squares.forEach(elm => elm.addEventListener("click", turn))
             }, 1200)
         }
         counter = 0
     } else {
         counter++
     }
+}
+// RESTART DO JOGO
+
+document.querySelector("button").onclick = () => {
+
 }
